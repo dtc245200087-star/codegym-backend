ab98075d5387a6e4938bd66d7be4afb6d1af6b91
dtc245200087-star <dtc245200087@ictu.edu.vn>
2026-09-24 22:17:51 +0700

    Thuc hanh truy van du lieu quan ly sinh vien

--- Quan_Ly_Sinh_Vien1/quan_ly_sinh_vien.sql
+++ Quan_Ly_Sinh_Vien1/quan_ly_sinh_vien.sql
@@ -75,3 +75,33 @@ VALUES
 (1, 1, 8, 1),
 (1, 2, 10, 2),
 (2, 1, 12, 1);
+-- ===================================================
+-- TRUY VẤN DỮ LIỆU (SELECT QUERY)
+-- ===================================================
+
+-- 1. Hiển thị danh sách tất cả các học viên
+SELECT *
+FROM Student;
+
+-- 2. Hiển thị danh sách các học viên đang theo học (Status = true/1)
+SELECT *
+FROM Student
+WHERE Status = true;
+
+-- 3. Hiển thị danh sách các môn học có thời gian học (Credit) nhỏ hơn 10
+SELECT *
+FROM Subject
+WHERE Credit < 10;
+
+-- 4. Hiển thị danh sách học viên lớp A1
+SELECT S.StudentId, S.StudentName, C.ClassName
+FROM Student S 
+JOIN Class C ON S.ClassId = C.ClassID
+WHERE C.ClassName = 'A1';
+
+-- 5. Hiển thị điểm môn CF của các học viên
+SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
+FROM Student S 
+JOIN Mark M ON S.StudentId = M.StudentId 
+JOIN Subject Sub ON M.SubId = Sub.SubId
+WHERE Sub.SubName = 'CF';
